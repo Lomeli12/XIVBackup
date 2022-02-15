fc168feb9f4ad08aacf1a8c9c42d19cd2193c4d3
Anthony Lomeli <anthonylomeli11@gmail.com>
2022-02-14 20:16:45 -0800

    Stop the nullable warning on build

--- XIVBackup/I18n.cs
+++ XIVBackup/I18n.cs
@@ -3,9 +3,11 @@
 namespace XIVBackup; 
 
 public class I18n {
+    private I18n() {}
+    
     public static string localize(string localization) => localization.Replace(@"\n", Environment.NewLine);
 
-    public static string localize(string localization, object? objs) {
+    public static string localize(string localization, params object[] objs) {
         localization = localize(localization);
         return objs != null ? string.Format(localization, objs) : localization;
     }
