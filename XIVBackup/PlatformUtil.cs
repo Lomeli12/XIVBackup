cac18f890c76cc1b67adad65f816abc2956235bf
Anthony Lomeli <anthonylomeli11@gmail.com>
2022-09-21 11:53:02 -0700

    Cleaned up PlatformUtil.cs and added support for the XIVLauncher flatpak

--- XIVBackup/PlatformUtil.cs
+++ XIVBackup/PlatformUtil.cs
@@ -7,65 +7,68 @@ namespace XIVBackup;
 public static class PlatformUtil {
     private const string GAMES_DIR = "My Games";
     private const string FF_DIR = "FINAL FANTASY XIV - A Realm Reborn";
+    
+    // XIVLauncher directory names
+    private const string XIV_WIN_CONFIG = "XIVLauncher";
+    private const string XIV_PLUGIN_CONFIG = "pluginConfigs";
 
     // Wine Bottle directory names
     private const string WINE_DRIVE = "drive_c";
     private const string WINE_USERS = "users";
     private const string WINE_DOCS = "My Documents";
+    
+    // XIVLauncher flatpak directory names
+    private const string XIV_LINUX_CONFIG = ".xlcore";
+    private const string XIV_GAME_CONFIG = "ffxivConfig";
 
     public static string getFFConfigPath() {
         if (Directory.GetCurrentDirectory().EndsWith(FF_DIR))
             return Directory.GetCurrentDirectory();
-        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-            return Path.Combine(
-                Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), GAMES_DIR), FF_DIR);
-        if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX)) {
-            var path =  Path.Combine(
-                Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), GAMES_DIR), FF_DIR);
-            if (!Directory.Exists(path)) {
-                path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
-                    Path.Combine("Library",
-                        Path.Combine("Application Support",
-                            Path.Combine("XIV on Mac",
-                                Path.Combine("game",
-                                    Path.Combine(WINE_DRIVE,
-                                        Path.Combine(WINE_USERS,
-                                            Path.Combine("emet-selch", //I really like that's what XIVMac uses lol
-                                                Path.Combine(WINE_DOCS,
-                                                    Path.Combine(GAMES_DIR, FF_DIR)
-                                                )
-                                            )
-                                        )
-                                    )
-                                )
-                            )
-                        )
-                    )
-                );
+        // Default FF config folder path
+        var path = multiCombinePath(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), 
+            GAMES_DIR, FF_DIR);
+        if (!Directory.Exists(path)) {
+            var newPath = path;
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) {
+                // XIVLauncher flatpak
+                newPath = combineAndVerifyPath(path, Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+                    XIV_LINUX_CONFIG, XIV_GAME_CONFIG);
+                if (newPath.Equals(path)) {
+                    // Lutris
+                    newPath = combineAndVerifyPath(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+                        "Games", "final-fantasy-xiv-online", WINE_DRIVE, WINE_USERS, Environment.UserName, 
+                        WINE_DOCS, GAMES_DIR, FF_DIR);
+                }
+            } else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX)) {
+                // Assuming they're using XIVMac if they're not using the vanilla launcher
+                newPath = combineAndVerifyPath(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+                    "Library", "Application Support", "XIV on Mac", "game", WINE_DRIVE, WINE_USERS, 
+                    "emet-selch", WINE_DOCS, GAMES_DIR, FF_DIR); // I like that the user is "Emet-Selch"
             }
 
-            return path;
+            if (!string.IsNullOrWhiteSpace(newPath) && newPath.Equals(path)) {
+                //TODO: Prompt to provide folder path 
+            } else path = newPath;
         }
-        if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) {
-            // We're just assuming they're using lutris
-            // Also this looks ugly AF
-            return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
-                Path.Combine("Games",
-                    Path.Combine("final-fantasy-xiv-online",
-                        Path.Combine(WINE_DRIVE,
-                            Path.Combine(WINE_USERS,
-                                Path.Combine(Environment.UserName,
-                                    Path.Combine(WINE_DOCS,
-                                        Path.Combine(GAMES_DIR, FF_DIR)
-                                    )
-                                )
-                            )
-                        )
-                    )
-                )
-            );
+        return path;
+    }
+
+    private static string combineAndVerifyPath(string basePath, params string[] folders) {
+        var newPath = multiCombinePath(folders);
+        return Directory.Exists(newPath) ? newPath : basePath;
+    }
+
+    private static string multiCombinePath(params string[] folders) {
+        switch (folders.Length) {
+            case 0:
+                return "";
+            case <= 1:
+                return folders[0];
         }
 
-        return null;
+        var path = folders[0];
+        for (var i = 1; i < folders.Length; i++)
+            path = Path.Combine(path, folders[i]);
+        return path;
     }
 }
